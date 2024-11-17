9a6f7d1364e564249e0d47d781b391347b288c60
ibrahim hisham <muhammadibrahimhisham@gmail.com>
2024-11-17 21:28:31 +0500

    implement abstract ManagerPlugin class

---
+++ Runtime/Manager Plugins/ManagerPlugin.cs
@@ -0,0 +1,8 @@
+﻿using Ibralogue.Parser;
+using UnityEngine;
+
+public abstract class ManagerPlugin : MonoBehaviour
+{
+    public abstract void Display(Conversation currentConversation, int lineIndex);
+    public abstract void Clear(Conversation currentConversation, int lineIndex);
+}
